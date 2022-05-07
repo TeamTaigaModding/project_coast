de98ae4c96563a5835f0e605d6ddf528f47212ad
dolthhaven <asphaltpotatofy@gmail.com>
2022-05-06 22:58:52 -0700

    test test test

--- src/main/java/com/dolthhaven/project_coast/common/blocks/BlockProperties.java
+++ src/main/java/com/dolthhaven/project_coast/common/blocks/BlockProperties.java
@@ -6,6 +6,7 @@ import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 
 public class BlockProperties {
+
     public static final BlockBehaviour.Properties SEAGULL_NEST = BlockBehaviour.Properties.of(Material.PLANT, MaterialColor.COLOR_BROWN).strength(0.8F).sound(SoundType.CROP);
 
     public static final BlockBehaviour.Properties SEAGRASS_BLOCK = BlockBehaviour.Properties.of(Material.SAND, MaterialColor.COLOR_BROWN).strength(0.8F).sound(SoundType.SAND);
